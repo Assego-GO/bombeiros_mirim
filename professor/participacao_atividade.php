cb52fb38cd1cc02ac535eeef9eecf708657740ff
Filipe <filipe_rapex@hotmail.com>
2025-06-09 14:51:05 +0000

    estilizacao da pagina de participacao de atividades

--- professor/participacao_atividade.php
+++ professor/participacao_atividade.php
@@ -107,7 +107,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registrar Participação - <?php echo htmlspecialchars($atividade['nome_atividade']); ?></title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" type="text/css" href="./css/style.css"/>
+    <link rel="stylesheet" type="text/css" href="./css/participacao.css"/>
     <style>
         .participacao-container {
             max-width: 1200px;
